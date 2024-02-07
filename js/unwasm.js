357a4b58e6d54cca3abdeaeff7662646ac29051a
X <i@jex.me>
2024-02-08 04:20:11 +0800

    Update unwasm.js(remove side effect)

--- js/unwasm.js
+++ js/unwasm.js
@@ -1,7 +1,8 @@
 import wasm from "./md4w.wasm";
 import { initWasm } from "./md4w.js";
 
-initWasm(wasm);
+export function init() {
+  initWasm(wasm);
+}
 
-export function init() {}
 export * from "./md4w.js";
