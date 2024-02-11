f6b4650a31bc95d3192f0b8b0ecb33606002a7ee
X <i@jex.me>
2024-02-11 22:08:50 +0800

    Update benchmark script

--- test/benchmark.js
+++ test/benchmark.js
@@ -1,18 +1,24 @@
 import MarkdownIt from "https://esm.sh/markdown-it@14.0.0";
+import { init as initMarkdownWasm, parse } from "https://esm.sh/markdown-wasm-es@1.2.1";
 import { init, mdToHtml } from "../js/index.js";
 
-const markdownit = MarkdownIt();
 await init();
-
+await initMarkdownWasm();
+const markdownit = MarkdownIt();
 const md = await Deno.readFile(new URL("commonmark-spec.md", import.meta.url));
 const mdStr = new TextDecoder().decode(md);
 
 // warmup runs
 mdToHtml(md);
+parse(md);
 markdownit.render(mdStr);
 
-Deno.bench("md4c.wasm", () => {
+Deno.bench("md4w", () => {
   mdToHtml(md);
+});
+
+Deno.bench("markdown-wasm", () => {
+  parse(md);
 });
 
 Deno.bench("markdown-it", () => {
